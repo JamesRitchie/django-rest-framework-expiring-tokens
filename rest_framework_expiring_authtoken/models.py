cfdb8c7ce9cea8034909a8e22b5f6cb5e5998b31
James Ritchie <james.a.ritchie@gmail.com>
2015-03-29 16:32:09 +0100

    Adding model

---
+++ rest_framework_expiring_authtoken/models.py
@@ -0,0 +1,25 @@
+"""Expiring Token models.
+
+Classes:
+    ExpiringToken
+"""
+
+from django.conf import settings
+from django.utils import timezone
+
+from rest_framework.authtoken.models import Token
+
+
+class ExpiringToken(Token):
+
+    """Extend Token to add an expired method."""
+
+    class Meta:
+        proxy = True
+
+    def expired(self):
+        """Return boolean inidcating token expiration."""
+        now = timezone.now()
+        if self.created < now - settings.EXPIRING_TOKEN_LIFESPAN:
+            return True
+        return False
