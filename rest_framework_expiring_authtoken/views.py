68ecf4ed07976a5fe48330d8ee0c189e3e10527e
tng10 <julio.zero@gmail.com>
2015-09-01 11:44:29 -0300

    Update views.py
    Use HTTP Basic Auth (GET)
    Fix request.DATA has been deprecated in DRF 3.2 (POST)

--- rest_framework_expiring_authtoken/views.py
+++ rest_framework_expiring_authtoken/views.py
@@ -7,6 +7,7 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.authentication import BasicAuthentication
 
 from rest_framework_expiring_authtoken.models import ExpiringToken
 
@@ -16,10 +17,31 @@ class ObtainExpiringAuthToken(ObtainAuthToken):
     """View enabling username/password exchange for expiring token."""
 
     model = ExpiringToken
+    authentication_classes = (BasicAuthentication, )
+    
+    def get(self, request):
+        """Respond to GET username/password(Header - HTTP Basic Auth) with token."""
+
+        if request.user:
+            token, _ = ExpiringToken.objects.get_or_create(
+                user=request.user
+            )
+
+            if token.expired():
+                # If the token is expired, generate a new one.
+                token.delete()
+                token = ExpiringToken.objects.create(
+                    user=request.user
+                )
+
+            data = {'token': token.key}
+            return Response(data)
+
+        return Response({'detail': "Couldn't find user"}, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request):
         """Respond to POSTed username/password with token."""
-        serializer = AuthTokenSerializer(data=request.DATA)
+        serializer = AuthTokenSerializer(data=request.data)
 
         if serializer.is_valid():
             token, _ = ExpiringToken.objects.get_or_create(
