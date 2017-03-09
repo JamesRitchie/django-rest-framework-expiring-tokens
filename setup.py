93c736725d8eb66ef596172d5066ca54b77c8870
requires.io <support@requires.io>
2017-03-09 15:03:08 +0000

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     license='BSD',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
-        'djangorestframework>=3.5,<3.6'
+        'djangorestframework>=3.6,<3.7'
     ],
     test_suite='runtests.run',
     tests_require=[
