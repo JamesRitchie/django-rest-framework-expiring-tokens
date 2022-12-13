281da537765f53203db67a80ccd018c52866232a
jeff <jeff@mighty.com>
2022-12-13 11:34:20 -0500

    Replace deprecated url import

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,8 @@
 """URL conf for testing Expiring Tokens."""
-from django.conf.urls import url
+from django.urls import re_path
 
 from rest_framework_expiring_authtoken.views import obtain_expiring_auth_token
 
 urlpatterns = [
-    url(r'^obtain-token/$', obtain_expiring_auth_token),
+    re_path(r'^obtain-token/$', obtain_expiring_auth_token),
 ]
